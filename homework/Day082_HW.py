4b9f0e1921b7868c078aea818266f4306d0b7293
Martin lee <li70534@gmail.com>
2019-03-30 22:38:11 +0800

    Add files via upload

---
+++ homework/Day082_HW.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Work
+# 1. 請比較使用不同層數以及不同 Dropout rate 對訓練的效果
+# 2. 將 optimizer 改成使用 Adam 並加上適當的 dropout rate 檢視結果
+
+# In[ ]:
+
+
+import os
+import keras
+import itertools
+# Disable GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+# In[ ]:
+
+
+train, test = keras.datasets.cifar10.load_data()
+
+
+# In[ ]:
+
+
+## 資料前處理
+def preproc_x(x, flatten=True):
+    x = x / 255.
+    if flatten:
+        x = x.reshape((len(x), -1))
+    return x
+
+def preproc_y(y, num_classes=10):
+    if y.shape[-1] == 1:
+        y = keras.utils.to_categorical(y, num_classes)
+    return y    
+
+
+# In[ ]:
+
+
+x_train, y_train = train
+x_test, y_test = test
+
+# Preproc the inputs
+x_train = preproc_x(x_train)
+x_test = preproc_x(x_test)
+
+# Preprc the outputs
+y_train = preproc_y(y_train)
+y_test = preproc_y(y_test)
+
+
+# In[ ]:
+
+
+
+from keras.layers import Dropout
+def build_mlp(input_shape, output_units=10, num_neurons=[512, 256, 128,256], drp_ratio=0.4):
+    input_layer = keras.layers.Input(input_shape)
+    
+    for i, n_units in enumerate(num_neurons):
+        if i == 0:
+            x = keras.layers.Dense(units=n_units, 
+                                   activation="relu", 
+                                   name="hidden_layer"+str(i+1))(input_layer)
+            x = Dropout(drp_ratio)(x)
+        else:
+            x = keras.layers.Dense(units=n_units, 
+                                   activation="relu", 
+                                   name="hidden_layer"+str(i+1))(x)
+            x = Dropout(drp_ratio)(x)
+    
+    out = keras.layers.Dense(units=output_units, activation="softmax", name="output")(x)
+    
+    model = keras.models.Model(inputs=[input_layer], outputs=[out])
+    return model
+
+
+# In[ ]:
+
+
+## 超參數設定
+LEARNING_RATE = 1e-4
+EPOCHS = 50
+BATCH_SIZE = 256
+MOMENTUM = 0.95
+Dropout_EXP = [0.1, 0.2, 0.4]
+LAYER_NEURONS = [[128, 128, 128], [128, 256, 256], [128, 256, 512]]
+
+# In[ ]:
+
+results = {}
+for i, (layer_neurons, drp_rate) in enumerate(itertools.product(LAYER_NEURONS, Dropout_EXP)):
+    print("Numbers of exp: %i, layer: %s, dropout_rate: %.2f" % (i, layer_neurons, drp_rate))
+    model = build_mlp(input_shape=x_train.shape[1:], num_neurons=layer_neurons, drp_ratio=drp_rate)
+    model.summary()
+    optimizer = keras.optimizers.Adam(lr=LEARNING_RATE)
+    model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
+
+    model.fit(x_train, y_train, 
+              epochs=EPOCHS, 
+              batch_size=BATCH_SIZE, 
+              validation_data=(x_test, y_test), 
+              verbose=0,
+              shuffle=True)
+
+    # Collect results
+    exp_name_tag = ("exp-%s" % (i))
+    results[exp_name_tag] = {'train-loss': model.history.history["loss"],
+                             'valid-loss': model.history.history["val_loss"],
+                             'train-acc': model.history.history["acc"],
+                             'valid-acc': model.history.history["val_acc"]}
+
+
+# In[ ]:
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as mplcm
+import matplotlib.colors as colors
+%matplotlib inline
+NUM_COLORS = 20
+
+cm = plt.get_cmap('gist_rainbow')
+cNorm  = colors.Normalize(vmin=0, vmax=NUM_COLORS-1)
+scalarMap = mplcm.ScalarMappable(norm=cNorm, cmap=cm)
+color_bar = [scalarMap.to_rgba(i) for i in range(NUM_COLORS)]
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-loss'])),results[cond]['train-loss'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-loss'])),results[cond]['valid-loss'], '--', label=cond, color=color_bar[i])
+plt.title("Loss")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-acc'])),results[cond]['train-acc'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-acc'])),results[cond]['valid-acc'], '--', label=cond, color=color_bar[i])
+plt.title("Accuracy")
+plt.legend()
+plt.show()
+
