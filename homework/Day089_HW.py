f26c9b3ead4beb6e5ba570e360ae87d18c6a6797
Martin lee <li70534@gmail.com>
2019-04-05 23:13:35 +0800

    Add files via upload

---
+++ homework/Day089_HW.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Work
+# 1. 請自行定義一個 loss function, 為 0.3 * focal loss + 0.7 cross-entropy，訓練並比較結果
+# 
+
+# In[ ]:
+
+
+import os
+import keras
+
+# Disable GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+# In[ ]:
+
+
+train, test = keras.datasets.cifar10.load_data()
+
+
+# In[ ]:
+
+
+## 資料前處理
+def preproc_x(x, flatten=True):
+    x = x / 255.
+    if flatten:
+        x = x.reshape((len(x), -1))
+    return x
+
+def preproc_y(y, num_classes=10):
+    if y.shape[-1] == 1:
+        y = keras.utils.to_categorical(y, num_classes)
+    return y    
+
+
+# In[ ]:
+
+
+x_train, y_train = train
+x_test, y_test = test
+
+# Preproc the inputs
+x_train = preproc_x(x_train)
+x_test = preproc_x(x_test)
+
+# Preprc the outputs
+y_train = preproc_y(y_train)
+y_test = preproc_y(y_test)
+
+
+# In[ ]:
+
+
+from keras.layers import BatchNormalization
+def build_mlp(input_shape, output_units=10, num_neurons=[512, 256, 128]):
+    """
+    TO DO:
+    Build your own model
+    """
+    return model
+
+
+# In[ ]:
+
+
+## 超參數設定
+LEARNING_RATE = 1e-3
+EPOCHS = 25
+BATCH_SIZE = 1024
+MOMENTUM = 0.95
+
+
+# In[ ]:
+
+
+import tensorflow as tf
+import keras.backend as K
+
+"""
+TO DO:
+Write your combined loss
+"""
+def combined_loss():
+    """Define the customized loss."""
+
+ce_weights_list = [0., 0.3, 0.5, 0.7, 1]
+
+
+# In[ ]:
+
+
+import itertools
+results = {}
+
+for i, ce_w in enumerate(ce_weights_list):
+    print("Numbers of exp: %i, ce_weight: %.2f" % (i, ce_w))
+
+    model = build_mlp(input_shape=x_train.shape[1:])
+    model.summary()
+    optimizer = keras.optimizers.SGD(lr=LEARNING_RATE, nesterov=True, momentum=MOMENTUM)
+    """
+    TO DO:
+    Compile your model with customized loss
+    """
+
+    model.fit(x_train, y_train, 
+              epochs=EPOCHS, 
+              batch_size=BATCH_SIZE, 
+              validation_data=(x_test, y_test), 
+              shuffle=True
+             )
+    
+    # Collect results
+    exp_name_tag = ("exp-%s" % (i))
+    results[exp_name_tag] = {'train-loss': model.history.history["loss"],
+                             'valid-loss': model.history.history["val_loss"],
+                             'train-acc': model.history.history["acc"],
+                             'valid-acc': model.history.history["val_acc"]}
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as mplcm
+import matplotlib.colors as colors
+get_ipython().run_line_magic('matplotlib', 'inline')
+NUM_COLORS = len(results.keys())
+
+cm = plt.get_cmap('gist_rainbow')
+cNorm  = colors.Normalize(vmin=0, vmax=NUM_COLORS-1)
+scalarMap = mplcm.ScalarMappable(norm=cNorm, cmap=cm)
+color_bar = [scalarMap.to_rgba(i) for i in range(NUM_COLORS)]
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-loss'])),results[cond]['train-loss'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-loss'])),results[cond]['valid-loss'], '--', label=cond, color=color_bar[i])
+plt.title("Loss")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-acc'])),results[cond]['train-acc'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-acc'])),results[cond]['valid-acc'], '--', label=cond, color=color_bar[i])
+plt.title("Accuracy")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
