225cae763c43906e81ab908fc9998a5981d7ff84
Martin lee <li70534@gmail.com>
2019-04-15 15:18:30 +0800

    Add files via upload

---
+++ homework/Day098_Python_generator_HW.py
@@ -0,0 +1,81 @@
+
+# ## 作業
+
+# 請參考昨天的程式碼，將訓練資料讀取方式改寫成 Generator，
+# 並將原本的 model.fit 改為 model.fit_generator 來進行訓練。請參考 Keras [官方文件中 fit_generator 的說明](https://keras.io/models/sequential/)
+
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import RMSprop, Adam
+import os
+
+
+batch_size = 128 # batch 的大小，如果出現 OOM error，請降低這個值
+num_classes = 10 # 類別的數量，Cifar 10 共有 10 個類別
+epochs = 10 # 訓練的 epochs 數量
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+def cifar_generator(image_array, label_array, batch_size=32):
+    while True:
+        for indexs in range(0, len(image_array), batch_size):
+            images = image_array[indexs: indexs+batch_size]
+            labels = label_array[indexs: indexs+batch_size]
+            yield images, labels
+            
+            
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+
+cifar_gen = cifar_generator(x_train, y_train)
+
+history = model.fit_generator(cifar_gen,
+                              epochs=epochs,
+                              steps_per_epoch=int(50000/32),
+                              verbose=1,
+                              validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
