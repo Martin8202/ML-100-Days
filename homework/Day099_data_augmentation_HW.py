7e19dc92c21f634bb8c040224785f8a3b14cfd77
Martin lee <li70534@gmail.com>
2019-04-18 22:57:24 +0800

    Add files via upload

---
+++ homework/Day099_data_augmentation_HW.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+# 此函數會幫我們把多張影像畫成一張多宮格圖
+def img_combine(img, ncols=8, size=1, path=False):
+    from math import ceil
+    import matplotlib.pyplot as plt
+    import numpy as np
+    nimg = len(img)
+    nrows = int(ceil(nimg/ncols))
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True, figsize=(ncols*size,nrows*size))
+    if nrows == 0:
+        return
+    elif ncols == 1:
+        for r, ax in zip(np.arange(nrows), axes):
+            nth=r
+            if nth < nimg:
+                ax.imshow(img[nth], cmap='rainbow', vmin=0, vmax=1)
+                
+            ax.set_axis_off()
+    elif nrows == 1:
+        for c, ax in zip(np.arange(ncols), axes):
+            nth=c
+            if nth < nimg:
+                ax.imshow(img[nth], cmap='rainbow', vmin=0, vmax=1)
+            ax.set_axis_off()
+    else:
+        for r, row in zip(np.arange(nrows), axes):
+            for c, ax in zip(np.arange(ncols), row):
+                nth=r*ncols+c
+                if nth < nimg:
+                    ax.imshow(img[nth], cmap='rainbow', vmin=0, vmax=1)
+                ax.set_axis_off()
+    plt.show()
+
+
+# In[3]:
+
+
+# 讀取 Cifar-10 資料集
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+
+# In[4]:
+
+
+# 取前 32 張圖片做視覺化
+images = x_train[:32]
+img_combine(images)
+
+
+# In[6]:
+
+
+# 建立 ImageDataGenerator，並指定我們要做資料增強的數值範圍
+data_generator = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True)
+
+
+# In[7]:
+
+
+# 注意！！ ImageDataGenerator 是一個 Generator (生成器)! 對 Generator 不熟悉的同學請回到 Day098 做複習。
+# 使用 .flow 後，就會對我們的影像進行增強，再 call next 取出 generator 的圖像。(shuffle=False 因為我們希望圖像的順序不要改變，方便觀察。實際訓練時預設是 shuffle=True) 
+augmented_iamges = next(data_generator.flow(images, shuffle=False))
+img_combine(augmented_iamges.astype("int")) # 注意在訓練時神經網路時，圖像資料必須要是 float32，但在做視覺化時要轉為 int 才能順利畫圖。所以為了畫圖才把資料轉為 int
+
+
+# In[9]:
+
+
+# 因為隨機性的關係，所以一樣的圖像再經過一次 generator 後的結果不一定相同
+augmented_iamges = next(data_generator.flow(images, shuffle=False))
+img_combine(augmented_iamges.astype("int"))
+
+
+# ## 作業
+
+# 請使用 ImageDataGenerator 來進行 Cifar-10 資料集的訓練，並觀察不同的圖像增強方法是否會顯著影響訓練結果
+
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import RMSprop, Adam
+
+batch_size = 128 # batch 的大小，如果出現 OOM error，請降低這個值
+num_classes = 10 # 類別的數量，Cifar 10 共有 10 個類別
+epochs = 10 # 訓練的 epochs 數量
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+augment_generator = ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+
+history = model.fit_generator(augment_generator.flow(x_train, y_train, batch_size=batch_size),
+                    steps_per_epoch=int(len(x_train)/batch_size), # 一個 epochs 要執行幾次 update，通常是資料量除以 batch size
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+'''
+因為在資料中增加了隨機性，因此通常會需要更多的訓練時間來達到更好的結果，
+可以看到 Training loss 比起原本沒有使用資料增強的 Loss 來得高一些，
+讓模型盡可能地增加泛化性，所以在 test data 上面通常可以獲得更好的結果
+'''
